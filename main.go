4e4ecf64a0951c061f4f8543e57ad0cba3673f40
Georgii Antonevich <easylanding.online@gmail.com>
2024-05-01 09:10:49 +0300

    work with database2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/Masterminds/squirrel"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -12,9 +13,11 @@ func HelloWorld() string {
 }
 
 type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
 }
 
 func CreateUserTable() error {
@@ -26,10 +29,12 @@ func CreateUserTable() error {
 
 	sqlStmt :=
 		`CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  age INTEGER
- )`
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            age INTEGER,
+            email TEXT,
+            username TEXT
+        )`
 
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
@@ -40,6 +45,26 @@ func CreateUserTable() error {
 	return nil
 }
 
+func PrepareQuery(operation string, table string, user User) (string, []interface{}, error) {
+	var query string
+	var args []interface{}
+
+	switch operation {
+	case "insert":
+		query, args, _ = squirrel.Insert(table).Columns("username", "email").Values(user.Username, user.Email).ToSql()
+	case "select":
+		query, args, _ = squirrel.Select("id", "username", "email").From(table).Where(squirrel.Eq{"id": user.ID}).ToSql()
+	case "update":
+		query, args, _ = squirrel.Update(table).Set("username", user.Username).Set("email", user.Email).Where(squirrel.Eq{"id": user.ID}).ToSql()
+	case "delete":
+		query, args, _ = squirrel.Delete(table).Where(squirrel.Eq{"id": user.ID}).ToSql()
+	default:
+		return "", nil, fmt.Errorf("invalid operation")
+	}
+
+	return query, args, nil
+}
+
 func InsertUser(user User) error {
 	db, err := sql.Open("sqlite3", "users.db")
 	if err != nil {
@@ -47,31 +72,36 @@ func InsertUser(user User) error {
 	}
 	defer db.Close()
 
-	sqlStmt :=
-		`INSERT INTO users (name, age) VALUES (?, ?)`
-
-	_, err = db.Exec(sqlStmt, user.Name, user.Age)
+	query, args, err := PrepareQuery("insert", "users", user)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("User inserted successfully")
+	_, err = db.Exec(query, args...)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func SelectUser(id int) (User, error) {
-	var user User
-
+func SelectUser(userID int) (User, error) {
 	db, err := sql.Open("sqlite3", "users.db")
 	if err != nil {
-		return user, err
+		return User{}, err
 	}
 	defer db.Close()
 
-	row := db.QueryRow("SELECT id, name, age FROM users WHERE id = ?", id)
-	err = row.Scan(&user.ID, &user.Name, &user.Age)
+	var user User
+	query, args, err := PrepareQuery("select", "users", User{ID: userID})
 	if err != nil {
-		return user, err
+		return User{}, err
+	}
+
+	row := db.QueryRow(query, args...)
+	err = row.Scan(&user.ID, &user.Username, &user.Email)
+	if err != nil {
+		return User{}, err
 	}
 
 	return user, nil
@@ -84,34 +114,36 @@ func UpdateUser(user User) error {
 	}
 	defer db.Close()
 
-	sqlStmt :=
-		`UPDATE users SET name = ?, age = ? WHERE id = ?`
-
-	_, err = db.Exec(sqlStmt, user.Name, user.Age, user.ID)
+	query, args, err := PrepareQuery("update", "users", user)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("User updated successfully")
+	_, err = db.Exec(query, args...)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func DeleteUser(id int) error {
+func DeleteUser(userID int) error {
 	db, err := sql.Open("sqlite3", "users.db")
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	sqlStmt :=
-		`DELETE FROM users WHERE id = ?`
-
-	_, err = db.Exec(sqlStmt, id)
+	query, args, err := PrepareQuery("delete", "users", User{ID: userID})
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("User deleted successfully")
+	_, err = db.Exec(query, args...)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -122,7 +154,7 @@ func SelectAllUsers() ([]User, error) {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM users")
+	rows, err := db.Query("SELECT id, username, email FROM users")
 	if err != nil {
 		return nil, err
 	}
@@ -132,7 +164,7 @@ func SelectAllUsers() ([]User, error) {
 
 	for rows.Next() {
 		var user User
-		err := rows.Scan(&user.ID, &user.Name, &user.Age)
+		err := rows.Scan(&user.ID, &user.Username, &user.Email)
 		if err != nil {
 			return nil, err
 		}
@@ -144,6 +176,8 @@ func SelectAllUsers() ([]User, error) {
 
 func main() {
 	fmt.Println(HelloWorld())
+
+	var err error
 
 	allUsers, err := SelectAllUsers()
 	if err != nil {
